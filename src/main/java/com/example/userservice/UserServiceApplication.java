57b59a710c2b153110f73fe0ae5729b7819f8e69
leehyeonuk <hwlee@beintech.co.kr>
2023-07-19 16:51:58 +0900

    spring cloud config

--- src/main/java/com/example/userservice/UserServiceApplication.java
+++ src/main/java/com/example/userservice/UserServiceApplication.java
@@ -1,6 +1,7 @@
 package com.example.userservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.trace.http.HttpTrace;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
@@ -18,5 +19,4 @@ public class UserServiceApplication {
 	public BCryptPasswordEncoder passwordEncoder(){
 		return new BCryptPasswordEncoder();
 	}
-
 }
